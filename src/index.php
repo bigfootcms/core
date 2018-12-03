cd2ab78eafcb35ce2b3c1f693907a1795c1eeda5
Nicholas Maietta <maietta@users.noreply.github.com>
2018-12-02 19:19:14 -0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -54,7 +54,7 @@ class Bigfoot extends Prefab {
 						Base::instance()->set("plugin_path", str_replace(Base::instance()->get("ROOT"), "", dirname($loader)));
 						Base::instance()->set('PLUGINS', dirname($loader) .'/');
 						if ( file_exists(dirname($loader).'/plugin.ini') ) Base::instance()->config(dirname($loader).'/plugin.ini');
-						if ( file_exists(dirname($loader).'/plugin.class.php') ) include(dirname($loader).'/plugin.class.php');
+						if ( file_exists(dirname($loader).'/src/plugin.class.php') ) include(dirname($loader).'/src/plugin.class.php');
 						if ( file_exists($loader) ) include($loader);
 					}
 				}
